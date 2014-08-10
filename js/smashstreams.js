8bbfaeba567160543527e8d2bf2697eaed6865b1
mark <justbanter@gmail.com>
2014-08-09 17:38:34 -0700

    updates

--- js/smashstreams.js
+++ js/smashstreams.js
@@ -1,67 +1,64 @@
 $(document).ready(function() {
   $.getJSON('https://api.twitch.tv/kraken/streams?game=Super%20Smash%20Bros.&callback=?', function(d) {
     $.each(d.streams, function(i, stream){
-        $("#64Stream").append('<tr><td><a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'">'+stream.channel.display_name+'</a></td><td>'+stream.viewers+'</td></tr>');
+        // $("#64").append('<div class="col-xs-5 main-col-left"> <a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'">'+stream.channel.display_name+'</a></div><div class="col-xs-5 main-col-right">'+stream.viewers+'</div>');
+        $("#64").append('<a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'"><div class="col-xs-5 main-col-left"> '+stream.channel.display_name+' </div></a><div class="col-xs-5 main-col-right">'+stream.viewers+'</div>');
+
     });
   });
   $.getJSON('https://api.twitch.tv/kraken/streams?game=Super%20Smash%20Bros.%20Melee&callback=?', function(d) {
     $.each(d.streams, function(i, stream){
-        $("#meleeStream").append('<tr><td><a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'">'+stream.channel.display_name+'</a></td><td>'+stream.viewers+'</td></tr>');
+        // $("#melee").append('<div class="col-xs-5 main-col-left"> <a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'">'+stream.channel.display_name+'</a></div><div class="col-xs-5 main-col-right">'+stream.viewers+'</div>');
+        $("#melee").append('<a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'"><div class="col-xs-5 main-col-left"> '+stream.channel.display_name+' </div></a><div class="col-xs-5 main-col-right">'+stream.viewers+'</div>');
     });
   });
   $.getJSON('https://api.twitch.tv/kraken/streams?game=Super%20Smash%20Bros.%20Brawl&callback=?', function(d) {
     $.each(d.streams, function(i, stream){
-        $("#brawlStream").append('<tr><td><a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'">'+stream.channel.display_name+'</a></td><td>'+stream.viewers+'</td></tr>');
+        // $("#brawl").append('<div class="col-xs-5 main-col-left"> <a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'">'+stream.channel.display_name+'</a></div><div class="col-xs-5 main-col-right">'+stream.viewers+'</div>');
+        $("#brawl").append('<a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'"><div class="col-xs-5 main-col-left"> '+stream.channel.display_name+' </div></a><div class="col-xs-5 main-col-right">'+stream.viewers+'</div>');
     });
   });
   $.getJSON('https://api.twitch.tv/kraken/streams?game=Project%20M&callback=?', function(d) {
     $.each(d.streams, function(i, stream){
-        $("#pmStream").append('<tr><td><a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'">'+stream.channel.display_name+'</a></td><td>'+stream.viewers+'</td></tr>');
+        // $("#projectm").append('<div class="col-xs-5 main-col-left"> <a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'">'+stream.channel.display_name+'</a></div><div class="col-xs-5 main-col-right">'+stream.viewers+'</div>');
+        $("#projectm").append('<a href="'+stream.channel.url+'" target=_blank title="'+stream.channel.status+'"><div class="col-xs-5 main-col-left"> '+stream.channel.display_name+' </div></a><div class="col-xs-5 main-col-right">'+stream.viewers+'</div>');
     });
   });
 
-  $('.64').click(function(){
+  $('#btn-64').click(function(){
     $('#64').show();
-    $('th').css({"background-color":"#f5f5f5"});
+    $('.header-col').css({"background-color":"#f9f9f9"});
     $(this).css({"background-color":"#ffe"});
-    $('#Melee').hide();
-    $('#Brawl').hide();
-    $('#ProjectM').hide();
+    $('#melee').hide();
+    $('#brawl').hide();
+    $('#projectm').hide();
   });
 
-  $('.melee').click(function(){
-    $('th').css({"background-color":"#f5f5f5"});
-    $(this).css({"background-color":"#ffe"});
-    $('#64').hide();
-    $('#Melee').show();
-    $('#Brawl').hide();
-    $('#ProjectM').hide();
-  });
-
-  $('.brawl').click(function(){
-    $('th').css({"background-color":"#f5f5f5"});
+  $('#btn-melee').click(function(){
+    $('.header-col').css({"background-color":"#f9f9f9"});
     $(this).css({"background-color":"#ffe"});
     $('#64').hide();
-    $('#Melee').hide();
-    $('#Brawl').show();
-    $('#ProjectM').hide();
+    $('#melee').show();
+    $('#brawl').hide();
+    $('#projectm').hide();
   });
 
-  $('.pm').click(function(){
-    $('th').css({"background-color":"#f5f5f5"});
+  $('#btn-brawl').click(function(){
+    $('.header-col').css({"background-color":"#f9f9f9"});
     $(this).css({"background-color":"#ffe"});
     $('#64').hide();
-    $('#Melee').hide();
-    $('#Brawl').hide();
-    $('#ProjectM').show();
+    $('#melee').hide();
+    $('#brawl').show();
+    $('#projectm').hide();
   });
 
-  $('th').hover(function(){
-    $(this).css({"opacity":"0.6"});
-  },function(){
-    $(this).css({"opacity":"1"});
-  }
-
-  );
+  $('#btn-pm').click(function(){
+    $('.header-col').css({"background-color":"#f9f9f9"});
+    $(this).css({"background-color":"#ffe"});
+    $('#64').hide();
+    $('#melee').hide();
+    $('#brawl').hide();
+    $('#projectm').show();
+  });
 
 });
