85f43e57f14f139668093b62ec0b2826d955586e
mark <justbanter@gmail.com>
2014-08-09 22:42:42 -0700

    update

--- js/smashstreams.js
+++ js/smashstreams.js
@@ -8,70 +8,39 @@ $(document).ready(function() {
     return html_str;
   }
 
+  function get_api_data(url) {
+    $.getJSON(url, function(d) {
+      $.each(d.streams, function(i, stream) {
+        $('#' + key).append(create_html_string(stream));
+      });
+    });
+  }
+
   base_url = 'https://api.twitch.tv/kraken/streams?game=';
 
-  $.getJSON(base_url + 'Super%20Smash%20Bros.&callback=?',
-    function(d) {
-      $.each(d.streams, function(i, stream){
-        $("#64").append(create_html_string(stream));
-    });
-  });
+  apis = {
+    'SuperSmashBros': 'Super%20Smash%20Bros.&callback=?',
+    'SuperSmashBrosMelee': 'Super%20Smash%20Bros.%20Melee&callback=?',
+    'SuperSmashBrosBrawl': 'Super%20Smash%20Bros.%20Brawl&callback=?',
+    'ProjectM': 'Project%20M&callback=?'
+  };
 
-  $.getJSON(base_url + 'Super%20Smash%20Bros.%20Melee&callback=?',
-    function(d) {
-      $.each(d.streams, function(i, stream){
-        $("#melee").append(create_html_string(stream));
+  for (key in apis) {
+    $.getJSON(base_url + apis[key], function(d) {
+      $.each(d.streams, function(i, stream) {
+        s = stream.game.replace(/[\s.]/g, '');
+        $('#' + s).append(create_html_string(stream));
+      });
     });
-  });
+  }
 
-  $.getJSON(base_url + 'Super%20Smash%20Bros.%20Brawl&callback=?',
-    function(d) {
-      $.each(d.streams, function(i, stream){
-        $("#brawl").append(create_html_string(stream));
-    });
-  });
-
-  $.getJSON(base_url + 'Project%20M&callback=?',
-    function(d) {
-      $.each(d.streams, function(i, stream){
-        $("#projectm").append(create_html_string(stream));
-    });
-  });
-
-  $('#btn-64').click(function(){
-    $('#64').show();
+  $('.game').click(function() {
     $('.header-col').css({"background-color":"#f9f9f9"});
     $(this).css({"background-color":"#ffe"});
-    $('#melee').hide();
-    $('#brawl').hide();
-    $('#projectm').hide();
-  });
-
-  $('#btn-melee').click(function(){
-    $('.header-col').css({"background-color":"#f9f9f9"});
-    $(this).css({"background-color":"#ffe"});
-    $('#64').hide();
-    $('#melee').show();
-    $('#brawl').hide();
-    $('#projectm').hide();
-  });
-
-  $('#btn-brawl').click(function(){
-    $('.header-col').css({"background-color":"#f9f9f9"});
-    $(this).css({"background-color":"#ffe"});
-    $('#64').hide();
-    $('#melee').hide();
-    $('#brawl').show();
-    $('#projectm').hide();
-  });
-
-  $('#btn-pm').click(function(){
-    $('.header-col').css({"background-color":"#f9f9f9"});
-    $(this).css({"background-color":"#ffe"});
-    $('#64').hide();
-    $('#melee').hide();
-    $('#brawl').hide();
-    $('#projectm').show();
+    $('.gamelist').each(function() {
+        $(this).hide();
+    });
+    $('#' + this.title).show();
   });
 
 });
